1854d1d795df315e365443df67bc9b788b7c0641
Mohammad Usman Khan <mohammeduk@spartaglobal.co>
2017-09-01 18:39:08 +0100

    App working fully. Only requires back button on main app page...

--- server.rb
+++ server.rb
@@ -14,6 +14,15 @@ Mongoid.load! "mongoid.config"
 
 class MyApp < Sinatra::Base
   register Sinatra::Namespace
+
+  configure do
+     set :public_folder, 'public'
+     set :views        , 'views'
+     set :root         , File.dirname(__FILE__)
+   end
+
+  $counter = 0
+
   # Endpoints
   get '/' do
     $snippets = Snippet.all
@@ -47,12 +56,13 @@ class MyApp < Sinatra::Base
   end
 
   namespace '/api/v1' do
-
-    before do
-      content_type 'application/json'
-    end
+    #
+    # before do
+    #   content_type 'application/json'
+    # end
 
     helpers do
+
       def base_url
         @base_url ||= "#{request.env['rack.url_scheme']}://{request.env['HTTP_HOST']}"
       end
@@ -80,6 +90,7 @@ class MyApp < Sinatra::Base
     end
 
     get '/snippets' do
+      content_type 'application/json'
       snippets = Snippet.all
 
       [:question, :answer].each do |filter|
@@ -90,36 +101,59 @@ class MyApp < Sinatra::Base
     end
 
     get '/snippets/rand' do
+      content_type 'application/json'
+
+      # Random number code for snippets
+
+      # snippets = Snippet.all
+      #
+      # [:question, :answer].each do |filter|
+      #   snippets = snippets.send(filter, params[filter]) if params[filter]
+      # end
+      #
+      # rand = Random.new
+      # rand = rand(0..snippets.count - 1)
+      #
+      # response = snippets.map { |snippet| SnippetSerializer.new(snippet) }[rand]
+      # response.to_json
+
       snippets = Snippet.all
 
       [:question, :answer].each do |filter|
         snippets = snippets.send(filter, params[filter]) if params[filter]
       end
 
-      rand = Random.new
-      rand = rand(0..snippets.count - 1)
-
-      response = snippets.map { |snippet| SnippetSerializer.new(snippet) }[rand]
+      $counter += 1
+      @count = Snippet.count
+      if $counter == @count
+        $counter = 0
+      elsif $counter > @count
+        $counter = 0
+      end
+      response = snippets.map { |snippet| SnippetSerializer.new(snippet) }[$counter]
       response.to_json
 
     end
 
     get '/snippets/manage' do
+      content_type 'application/json'
       erb :manage_snippets
     end
 
     # Edit Snippets confirmation
     get '/snippets/edit' do
-      $edit_snippet = params[:snip_q]
+      # $edit_snippet = params[:snip_q]
       erb :edit_snippet
     end
 
     get '/snippets/:id' do |id|
+      content_type 'application/json'
       halt_if_not_found!
       serialize(snippet)
     end
 
     post '/snippets' do
+      content_type 'application/json'
       snippet = Snippet.new(json_params)
       halt 422, serialize(snippet) unless book.snippet
       response.headers['Location'] = "#{base_url}/api/v1/snippets/#{snippet.id}"
@@ -127,6 +161,7 @@ class MyApp < Sinatra::Base
     end
 
     post '/snippets/new' do
+      content_type 'application/json'
       # binding.pry
       param_q = params["question"]
       param_a = params["answer"]
@@ -139,12 +174,14 @@ class MyApp < Sinatra::Base
     end
 
     patch '/snippets/:id' do |id|
+      content_type 'application/json'
       halt_if_not_found!
       halt 422, serialize(snippet) unless snippet.update_attributes(json_params)
       serialize(snippet)
     end
 
     post '/snippets/delete/:id' do |id|
+      content_type 'application/json'
       id = params['id'].to_s
       Snippet.where(id: "#{id}").delete
       snippet.destroy if snippet
@@ -152,12 +189,27 @@ class MyApp < Sinatra::Base
       redirect '/snippets/manage'
     end
 
-    post '/snippets/edit/:id' do |id|
-      binding.pry
-      id = params['id'].to_s
-      Snippet.where(id: "#{id}").delete
-      snippet.destroy if snippet
-      status 204
+    # post '/snippets/edit/:id' do |id|
+    #   $id = id
+    #   $id = params['id'].to_s
+    #   a = Snippet.find("#{$id}")
+    #   $question = a['question']
+    #   $answer = a['answer']
+    #   redirect_to '/snippets/edit'
+    # # end
+
+    get '/snippets/edit/:id' do |id|
+      $id = id
+      @id = params['id'].to_s
+      a = Snippet.find("#{@id}")
+      @question = a['question']
+      @answer = a['answer']
+      erb :edit_snippet
+    end
+
+    post '/snippets/edited/' do
+      content_type 'application/json'
+      Snippet.where(:id => "#{$id}").update_all(:question => "#{params['question']}", :answer => "#{params['answer']}")
       redirect '/snippets/manage'
     end
   end
