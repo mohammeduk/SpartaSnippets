0f329768823e69a729e1521a70b69273eba9fea7
Mohammad Usman Khan <usman.khan@bjss.com>
2017-09-13 16:02:26 +0100

    Hide/show header on fullscreen toggle

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -10,10 +10,10 @@ $(function() {
 });
 
 function launchFullScreen(element) {
-  var x = document.getElementById('play-link');
-    x.style.display = 'none';
-  var x = document.getElementById('manage-link');
-    x.style.display = 'none';
+  // var x = document.getElementById('play-link');
+  //   x.style.display = 'none';
+  // var x = document.getElementById('manage-link');
+  //   x.style.display = 'none';
 
     if(element.requestFullScreen) {
       element.requestFullScreen();
@@ -22,17 +22,28 @@ function launchFullScreen(element) {
     } else if(element.webkitRequestFullScreen) {
       element.webkitRequestFullScreen();
     }
+    $('header').slideUp();
+    $('#play-link').slideUp();
   }
 
-  $("#play-link").click(function(){
-      $("#play-link").hide(1000);
-      $("#play-h1").hide(1000);
-      $("#manage-link").hide(1000);
-      $("#play-h3").hide(1000);
-  });
+  // $("#play-link").click(function(){
+  //     $("#play-link").hide(1000);
+  //     $("#play-h1").hide(1000);
+  //     $("#manage-link").hide(1000);
+  //     $("#play-h3").hide(1000);
+  // });
 
-  $("#play-link").click(function (element) {
-      // $("#play-link").hide();
-      launchFullScreen(document.documentElement);
-      // element.display = 'none';
-  })
+
+  $('#play-link').click(function (element) {
+        launchFullScreen(document.documentElement);
+    });
+
+    $(document).bind('webkitfullscreenchange mozfullscreenchange fullscreenchange', function(e) {
+        var state = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
+        var event = state ? 'FullscreenOn' : 'FullscreenOff';
+
+        if(event == 'FullscreenOff') {
+            $('header').slideDown();
+            $('#play-link').slideDown();
+        }
+    });
